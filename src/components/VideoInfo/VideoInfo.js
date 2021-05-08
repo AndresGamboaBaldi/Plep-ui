d72571f8fd9a03d7a454c182f91705cac8b1b9ee
Pedro Brañez <pedrobran8@gmail.com>
2021-05-08 12:48:25 -0400

    Applying style to VideoInfo and Video components

--- src/components/VideoInfo/VideoInfo.js
+++ src/components/VideoInfo/VideoInfo.js
@@ -15,25 +15,23 @@ export default function VideoInfo(
   const [open, setOpen] = useState(false);
   return (
     <Container fluid>
-      
           <ReactPlayer
             width="100%"
             controls
             url="https://youtu.be/odM92ap8_c0"
           />
-
-          <Card className="text-left">
-            <div id="module" class="card-block px-2">
+          <Card className="text-left info-card">
+            <div id="module" className="card-block">
               <Row noGutters>
                 <Col>
-                  <h4 class="card-title">Godzila vs Kong</h4>
+                  <Card.Title className="title-text">Godzila vs Kong</Card.Title>
                 </Col>
                 <Col>
-                  <p class="card-text">Action</p>
+                  <Card.Text className="genre-text">Action</Card.Text>
                 </Col>
               </Row>
               <Collapse in={open}>
-                <div id="video-description">
+                <Card.Text id="video-description">
                   Godzilla vs. Kong is a 2021 American monster film directed by
                   Adam Wingard. A sequel to Godzilla: King of the Monsters
                   (2019) and Kong: Skull Island (2017), it is the fourth film in
@@ -47,13 +45,14 @@ export default function VideoInfo(
                   Godzilla as humans lure the ape into the Hollow Earth to
                   retrieve a power source for a weapon to stop Godzilla's
                   mysterious rampages.
-                </div>
+                </Card.Text>
               </Collapse>
               <Button
                 variant="link"
                 onClick={() => setOpen(!open)}
                 aria-controls="video-description"
                 aria-expanded={open}
+                className="link-button"
               >{open ? "- Show Less" : "+ Show More"}
               </Button>
             </div>
