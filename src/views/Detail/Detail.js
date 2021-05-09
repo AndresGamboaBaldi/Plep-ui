e789adf73377f310efa4e67355f192056dde3d64
Pedro Brañez <pedrobran8@gmail.com>
2021-05-09 18:10:26 -0400

    Implemented connection with API to retrieve videos

--- src/views/Detail/Detail.js
+++ src/views/Detail/Detail.js
@@ -1,20 +1,36 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import Video from "../../components/Video/Video";
 import VideoInfo from "../../components/VideoInfo/VideoInfo";
 import "./Detail.css";
+import axios from "axios";
 
 export default function Detail() {
-  const mock = [1, 2, 3, 4, 5];
+  const [movies, setMovies] = useState([]);
+  useEffect(() => {
+    const getMovies = async () => {
+      try {
+        const { data } = await axios.get(
+          "https://plep.herokuapp.com/api/movies"
+        );
+        console.log(data);
+        setMovies(data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+
+    getMovies().catch(null);
+  }, []);
   return (
     <Container fluid className="body">
       <Row noGutters>
         <Col lg={8} md={12}>
-          <VideoInfo />
+          <VideoInfo data={movies[3]}/>
         </Col>
         <Col lg={4} md={12} className="mb-4">
-          {mock.map((data) => (
-            <Video />
+          {movies.map((data) => (
+            <Video data={data} key={data._id}/>
           ))}
         </Col>
       </Row>
